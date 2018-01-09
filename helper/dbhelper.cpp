6782c5c2db7026a49a7561c587509cdc52fc426a
Oleg Z <utamons@yandex.com>
2018-01-09 12:25:50 +0600

    Added decision table

--- helper/dbhelper.cpp
+++ helper/dbhelper.cpp
@@ -20,10 +20,10 @@ void DBHelper::close() {
 
 // Checking for database file. Creating this if needed.
 void DBHelper::checkDB() {
-
+	
 	QString dbPath = getWorkPath().append(QDir::toNativeSeparators("/tm2db.sqlite"));
 	QString dbBack = getWorkPath().append(QDir::toNativeSeparators("/tm2db.sqlite.bak"));
-
+	
 	QFile dbFile(dbPath);
 	QFile dbFileBack(dbBack);
 	if (dbFile.exists()) {
@@ -32,7 +32,7 @@ void DBHelper::checkDB() {
 		}
 		dbFile.copy(dbBack);
 	}
-
+	
 	db.setDatabaseName(dbPath);
 	if (db.open()) {
 		createStructure();
@@ -47,76 +47,96 @@ void DBHelper::checkDB() {
 }
 
 
-// Creating database structure.
 void DBHelper::createStructure() {
-
+	
 	//  Categories
 	const QString catTableSQL = "create table if not exists cat_rb ("
-	                            "id             integer primary key autoincrement," // Primary key
-	        "abbrev         char(20) not null unique,"// Abbreviation
-			"parent_id      integer,"// Ссылка на родительский узел
-	        "virtual        integer not null check(virtual=0 or virtual=1),"// 1 - virtual category, 0 - arbitrary category
-	        "name           varchar(80) not null unique,"
-	        "expanded       integer not null check(expanded=0 or expanded=1),"// Expanded in the tree view
-	        "comment		varchar(255)"
-			")";
-
-
+	                            "id				integer primary key autoincrement,"
+	                            "abbrev			char(20) not null unique,"
+	                            "parent_id		integer,"
+	                            "virtual		integer not null check(virtual=0 or virtual=1),"
+	                            "name			varchar(80) not null unique,"
+	                            "expanded		integer not null check(expanded=0 or expanded=1),"
+	                            "comment		varchar(255)"
+	                            ")";
+	
+	
 	// Units
 	const QString unitRbSQL = "create table if not exists unit_rb ("
-	                          "id            integer primary key autoincrement,"
-	        "name          varchar(50) not null unique"
-			")";
-
+	                          "id			integer primary key autoincrement,"
+	                          "name			varchar(50) not null unique"
+	                          ")";
+	
 	// Tags
 	const QString tagRbSQL = "create table if not exists tag_rb ("
-	                         "id            integer primary key autoincrement,"
-	        "name          varchar(50) not null unique"
-			")";
-
-
+	                         "id			integer primary key autoincrement,"
+	                         "name			varchar(50) not null unique"
+	                         ")";
+	
+	
 	const QString actTagLinkSQL = "create table if not exists act_tag_link ("
-								  "act_id        integer references actions (id) not null,"
-								  "tag_id        integer references tag_rb (id) not null,"
-								  "constraint    act_tag_link_pk primary key (act_id,tag_id))";
-
-    const QString catTagLinkSQL = "create table if not exists cat_tag_link ("
-								  "cat_id        integer references cat_rb (id) not null,"
-								  "tag_id        integer references tag_rb (id) not null,"
-								  "constraint    cat_tag_link_pk primary key (cat_id,tag_id))";
-
+	                              "act_id		integer references actions (id) not null,"
+	                              "tag_id		integer references tag_rb (id) not null,"
+	                              "constraint	act_tag_link_pk primary key (act_id,tag_id))";
+	
+	const QString catTagLinkSQL = "create table if not exists cat_tag_link ("
+	                              "cat_id		integer references cat_rb (id) not null,"
+	                              "tag_id		integer references tag_rb (id) not null,"
+	                              "constraint	cat_tag_link_pk primary key (cat_id,tag_id))";
+	
 	// Rates
 	const QString rateRbTableSQL = "create table if not exists rate_rb ("
-	                               "id            integer primary key autoincrement,"
-	        "name          varchar(80) not null unique,"
-	        "time          integer,"
-	        "comment	   varchar(255),"
-	        "unit_id       integer references unit_rb (id)"
-			")";
-
+	                               "id			integer primary key autoincrement,"
+	                               "name		varchar(80) not null unique,"
+	                               "time		integer,"
+	                               "comment		varchar(255),"
+	                               "unit_id		integer references unit_rb (id)"
+	                               ")";
+	
 	const QString catsRatesLinkSQL = "create table if not exists cat_rate_link ("
-	                                 "cat_id         integer references cat_rb (id) not null,"
-	        "rate_id        integer references rate_rb (id) not null,"
-	        "default_value  number not null, "
-			"constraint cat_rate_link_pk primary key (cat_id,rate_id))";
-
+	                                 "cat_id		integer references cat_rb (id) not null,"
+	                                 "rate_id		integer references rate_rb (id) not null,"
+	                                 "default_value	number not null, "
+	                                 "constraint	cat_rate_link_pk primary key (cat_id,rate_id))";
+	
 	// Actions
 	const QString actionsSQL = "create table if not exists actions ("
-	                           "id             integer primary key autoincrement,"
-	        "time           numeric not null, "// Start time
-	        "cat_id         integer references cat_rb (id) not null,"
-	        "mins           integer not null,"// Duration
-	        "comment        varchar(1024)"
-			")";
-
-
+	                           "id			integer primary key autoincrement,"
+	                           "time		numeric not null, "
+	                           "cat_id		integer references cat_rb (id) not null,"
+	                           "mins		integer not null,"
+	                           "comment		varchar(1024)"
+	                           ")";
+	
+	
 	const QString actRateLinkSQL = "CREATE TABLE  if not exists act_rate_link ("
-	                               "act_id  integer references actions (id) not null,"
-	        "rate_id integer references rate_rb (id) not null,"
-			"value   number not null, "
-			"constraint act_rate_link_pk primary key (act_id,rate_id)"
-			")";
+	                               "act_id		integer references actions (id) not null,"
+	                               "rate_id		integer references rate_rb (id) not null,"
+	                               "value		number not null, "
+	                               "constraint	act_rate_link_pk primary key (act_id,rate_id)"
+	                               ")";
+	
+	
+	const QString decisionsSQL = "create table if not exists decisions ("
+	                             "id			integer primary key autoincrement,"
+	                             "start_date	numeric not null, "
+	                             "end_date		numeric not null, "
+	                             "done			integer not null check(done=0 or done=1),"
+	                             "remark		varchar(1024)"
+	                             ")";
 
+	const QString decisionRateLinkSQL = "CREATE TABLE  if not exists decision_rate_link ("
+	                                    "decision_id		integer references decisions (id) not null,"
+	                                    "rate_id			integer references rate_rb (id) not null,"
+	                                    "value			number not null, "
+	                                    "constraint		decision_rate_link_pk primary key (decision_id,rate_id)"
+	                                    ")";
+
+	const QString decisionTagLinkSQL = "create table if not exists decision_tag_link ("
+	                                   "decision_id		integer references decision (id) not null,"
+	                                   "tag_id			integer references tag_rb (id) not null,"
+	                                   "constraint		decision_tag_link_pk primary key (decision_id,tag_id))";
+	
 	execQuery(catTableSQL);
 	execQuery(unitRbSQL);
 	execQuery(rateRbTableSQL);
@@ -124,8 +144,12 @@ void DBHelper::createStructure() {
 	execQuery(actionsSQL);
 	execQuery(actRateLinkSQL);
 	execQuery(actTagLinkSQL);
-    execQuery(tagRbSQL);
+	execQuery(tagRbSQL);
 	execQuery(catTagLinkSQL);
+	execQuery(decisionsSQL);
+	execQuery(decisionRateLinkSQL);
+	execQuery(decisionTagLinkSQL);
+
 }
 
 
@@ -176,29 +200,29 @@ void execQuery(QSqlQuery q, std::function<void()> f) {
 
 
 void beginTransaction() {
-    const QString error = "Cannot begin transaction (database does not support it?)";
-    if (!getDb().transaction()) {
-        qDebug() << error;
+	const QString error = "Cannot begin transaction (database does not support it?)";
+	if (!getDb().transaction()) {
+		qDebug() << error;
 		QMessageBox::critical(nullptr, QObject::tr("Database error"), error);
-        QApplication::exit(1);
-    }
+		QApplication::exit(1);
+	}
 }
 
 void commit() {
-    QSqlDatabase db = getDb();
-    if (!db.commit()) {
-        qDebug() << db.lastError();
+	QSqlDatabase db = getDb();
+	if (!db.commit()) {
+		qDebug() << db.lastError();
 		QMessageBox::critical(nullptr, QObject::tr("Database error"), db.lastError().text());
-        QApplication::exit(1);
-    }
+		QApplication::exit(1);
+	}
 }
 
 void rollback() {
-    QSqlDatabase db = getDb();
-    if (!db.rollback()) {
-        qDebug() << db.lastError();
+	QSqlDatabase db = getDb();
+	if (!db.rollback()) {
+		qDebug() << db.lastError();
 		QMessageBox::critical(nullptr, QObject::tr("Database error"), db.lastError().text());
-        QApplication::exit(1);
-    }
+		QApplication::exit(1);
+	}
 }
 
